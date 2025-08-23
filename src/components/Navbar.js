fa0a35890631ab44ae530015a74ec014e2d042ee
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-23 21:16:08 +0530

    nav to set

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,21 @@ const Navbar = () => {
     <nav
       className={`${
         darkMode ? "bg-darkBg" : "bg-lightBg"
-      } flex flex-row justify-between p-4`}
+      } flex flex-row justify-between p-4 items-center`}
     >
       <div className="flex">
-        <h1>Logo</h1>
+        <h1 className="p-2 dark:text-darkText text-[#262626] font-medium ">
+          Sachin's Development
+        </h1>
       </div>
-      <div className="flex flex-row gap-2">
-        <Link to={"/signup"}>SignUp</Link>
-      </div>
-      <div>
+      <div className="flex flex-row gap-4">
+        <button className="bg-[#262626] p-2 dark:text-darkText text-[#f6f6f6] font-medium rounded-xl hover:bg-[#333]">
+          <Link to={"/signup"}>SignUp</Link>
+        </button>
+
+        <button className="bg-[#262626] p-2 dark:text-darkText text-[#f6f6f6] font-medium rounded-xl hover:bg-[#333]">
+          <Link to={"/signup"}>Login</Link>
+        </button>
         <ThemeToggle darkMode={darkMode} setDarkMode={toggleDarkMode} />
       </div>
     </nav>
