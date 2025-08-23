5f9ad764fb7f18699740f35dbb3290ddbeb20f0c
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-23 21:26:06 +0530

    persist for local

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import useThemeStore from "../store/themeStore";
+
 
 const SignupPage = () => {
+  const darkMode = useThemeStore((state) => state.darkMode);
+
   return (
-    <div>
-        SignupPage
-      
-    </div>
-  )
+    <main className={`${darkMode ? "dark" : ""} min-h-screen`}>
+      <div className="flex flex-row align-center justify-center gap-6 p-2 h-full w-full">
+        <div className="flex  align-center">
+          <form action="Submit" className="flex flex-col">
+            <div className="flex flex-row gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="">First Name</label>
+                <input type="text" name="" id="" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="">Last Name</label>
+                <input type="text" name="" id="" />
+              </div>
+            </div>
+          </form>
+        </div>
+        {/* right pannel  */}
+        <div>
+          <h1>Right Pannel</h1>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default SignupPage
