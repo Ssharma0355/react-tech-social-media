d5c5e98a0eb211f5fb1a0673ac712df6eff33dcb
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-23 10:10:44 +0530

    set-up for development

---
+++ src/Widgets/ThemeToggle.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+const ThemeToggle = ({darkMode, setDarkMode}) => {
+    useEffect(() => {
+      const saved = localStorage.getItem("darkMode") === "true";
+      setDarkMode(saved);
+    }, []);
+
+  return (
+    <div>
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        className="px-4 py-2 rounded bg-gray-300 dark:bg-gray-700 text-black dark:text-white transition-colors"
+      >
+        {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      </button>
+    </div>
+  );
+}
+
+export default ThemeToggle
