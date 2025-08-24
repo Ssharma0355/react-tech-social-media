7e8a7f84ac1f2590d29f10708f98767264ca2af4
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-24 13:47:23 +0530

    sign up form

--- src/pages/authpage/SignupPage.js
+++ src/pages/authpage/SignupPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import useThemeStore from "../../store/themeStore";
 import Navbar from "../../components/Navbar";
 import NavLayout from "../../layouts/NavLayout";
+import CaruselSection from "../landingpage/Section1/CaruselSection";
 
 const SignupPage = () => {
   const darkMode = useThemeStore((state) => state.darkMode);
@@ -9,25 +10,65 @@ const SignupPage = () => {
   return (
     <NavLayout>
       <main className={`${darkMode ? "dark" : ""}`}>
-        <div className="flex flex-row align-center justify-center gap-6 p-2  w-full">
-          <div className="flex  align-center">
-            <form action="Submit" className="flex flex-col">
-              <div className="flex flex-row gap-4">
-                <div className="flex flex-col">
+        <div className="flex flex-row justify-center align-center justify-center gap-6 px-12 py-8 w-full">
+          <div className="flex flex-col align-center w-full p-4 gap-8">
+            <div className="flex align-center justify-center">
+              <button className="w-full bg-gray-200 rounded-[8px] p-1">
+                Signup with Google
+              </button>
+            </div>
+            <form action="Submit" className="flex flex-col gap-4">
+              <div className="flex flex-row gap-4 w-full">
+                <div className="flex flex-col gap-1 w-full">
                   <label htmlFor="">First Name</label>
-                  <input type="text" name="" id="" />
+                  <input
+                    className="p-1 rounded-[8px]"
+                    type="text"
+                    name=""
+                    id=""
+                  />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col gap-1 w-full">
                   <label htmlFor="">Last Name</label>
-                  <input type="text" name="" id="" />
+                  <input
+                    className="p-1 rounded-[8px]"
+                    type="text"
+                    name=""
+                    id=""
+                  />
                 </div>
               </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="">Email</label>
+                <input
+                  className="p-1 rounded-[8px]"
+                  type="text"
+                  name=""
+                  id=""
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="">Password</label>
+                <input
+                  className="p-1 rounded-[8px]"
+                  type="text"
+                  name=""
+                  id=""
+                />
+              </div>
+              <button className="w-full bg-gray-200 rounded-[8px] p-1">
+                Signup with email
+              </button>
             </form>
+            <div>
+              <p>
+                You already have account?{" "}
+                <span className="text-[#4CAF50] font-bold">Login</span>
+              </p>
+            </div>
           </div>
           {/* right pannel  */}
-          <div>
-            <h1>Right Pannel</h1>
-          </div>
+          <CaruselSection />
         </div>
       </main>
     </NavLayout>
--- src/pages/landingpage/Section2/Section2.js
+++ src/pages/landingpage/Section2/Section2.js
@@ -1,22 +1,15 @@
 import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+
 
 const Section2 = () => {
   return (
     <main className='flex flex-col'>
         <div className='flex flex-row gap4'>
-            <div>
-                LinkedIn
+            <div className='flex flex-row'>
+                <FaLinkedin />
             </div>
-            <div>
-                InstaHire
-            </div>
-            <div>
-                Naukari
-            </div>
-
         </div>
-
-      
     </main>
   )
 }
