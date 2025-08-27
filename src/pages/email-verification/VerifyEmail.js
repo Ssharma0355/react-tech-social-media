31dd1901f15690c630d0cefa8273e77f93276fb7
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-27 10:33:35 +0530

    added route and floe

--- src/pages/email-verification/VerifyEmail.js
+++ src/pages/email-verification/VerifyEmail.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+
 
 const VerifyEmail = () => {
+    const navigate = useNavigate();
+
+    const verfyEmail =()=>{
+        navigate("/onboarding")
+
+    }
   return (
     <div>
       otp and step to onboarding
+      <button className='p-2 bg-black text-yellow-50' onClick={verfyEmail}>Verify</button>
     </div>
   )
 }
--- src/pages/onboarding/commonUser/PersonalInfo.js
+++ src/pages/onboarding/commonUser/PersonalInfo.js
@@ -4,6 +4,8 @@ const PersonalInfo = () => {
   return (
     <div>
       <h1>PersonalInfo</h1>
+      name and email from sign in store in zustand
+      
     </div>
   );
 }
