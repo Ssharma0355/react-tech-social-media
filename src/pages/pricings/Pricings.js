a947ccbc2eee8616718ec299492ddf727402a3ee
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-08-27 11:41:50 +0530

    making pricing page

--- src/pages/pricings/Pricings.js
+++ src/pages/pricings/Pricings.js
@@ -5,7 +5,13 @@ import NavLayout from '../../layouts/NavLayout';
 const Pricings = () => {
   return (
     <NavLayout>
-      Pricing
+      <main className="flex flex-col justify-center px-12 py-8">
+        <div className="flex flex-col text-center ">
+          <h1 className="text-[#4CAF50] text-[32px]">Your Dream is priceless?</h1>
+          <h2 className='text-[24px]'>But we are budget friendly!!</h2>
+        </div>
+        <h1>comming soon</h1>
+      </main>
     </NavLayout>
   );
 }
