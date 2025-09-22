927f4ddea6ebd9521be99b359a5aa5bcab5c8be0
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-09-22 11:45:08 +0530

    added section2 and 3

--- src/pages/landingpage/LandingPage.js
+++ src/pages/landingpage/LandingPage.js
@@ -4,6 +4,7 @@ import NavLayout from "../../layouts/NavLayout";
 import CaruselSection from "./Section1/CaruselSection";
 import Section1 from "./Section1/Section1";
 import Section2 from "./Section2/Section2";
+import Section3 from "./Section3/Section3";
 
 const LandingPage = () => {
   return (
@@ -14,6 +15,7 @@ const LandingPage = () => {
 
         {/* Other Sections */}
         <Section2 />
+        <Section3 />
       </main>
     </NavLayout>
   );
